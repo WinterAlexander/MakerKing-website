ab7e96c05cb6680fceeb3e5a8ff41afa70bef1fc
Alexander Winter <a.w1nter@hotmail.com>
2018-08-12 18:22:53 -0400

    Making footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from "./header/header.component";
 import { HomeComponent } from "./home/home.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @NgModule({
   declarations: [
-    AppComponent, HeaderComponent, HomeComponent
+    AppComponent, HeaderComponent, HomeComponent, FooterComponent
   ],
   imports: [
     BrowserModule
