d80fef1832db1edc56b20a0aae1cd02fc3a14a34
Alexander Winter <a.w1nter@hotmail.com>
2019-02-26 15:25:20 -0500

    Updated discord invitation link

--- src/app/social-constants.ts
+++ src/app/social-constants.ts
@@ -1,4 +1,4 @@
-export const DISCORD_ADDRESS: string = "https://discord.gg/r3v3WPM";
+export const DISCORD_ADDRESS: string = "https://discord.gg/Guv4UYn";
 export const TWITTER_ADDRESS: string = "https://twitter.com/jumpaigame";
 export const REDDIT_ADDRESS: string = "https://www.reddit.com/r/jumpai/";
 export const FACEBOOK_ADDRESS: string = "https://www.facebook.com/jumpaigame";
