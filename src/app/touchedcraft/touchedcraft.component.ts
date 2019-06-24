d11e1c9b7392a830243b575635287a0edf127720
Alexander Winter <a.w1nter@hotmail.com>
2019-06-24 17:18:58 -0400

    Making touchedcraft component

---
+++ src/app/touchedcraft/touchedcraft.component.ts
@@ -0,0 +1,19 @@
+import {Component, OnInit} from '@angular/core';
+import {Title} from '@angular/platform-browser';
+import {ActivatedRoute, Router} from '@angular/router';
+
+@Component({
+	selector: 'touchedcraft',
+	templateUrl: './touchedcraft.component.html',
+	styleUrls: ['./touchedcraft.component.css']
+})
+export class TouchedcraftComponent implements OnInit {
+
+	constructor(private title: Title,
+	            private activatedRoute: ActivatedRoute,
+	            private router: Router) {}
+
+	ngOnInit() {
+
+	}
+}
