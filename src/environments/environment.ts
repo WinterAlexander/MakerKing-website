2179e308009c6dc146270ad5e5db567f71a40267
Alexander Winter <a.w1nter@hotmail.com>
2025-07-16 20:32:15 -0400

    Improved look

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,5 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-	production: false,
-	server: 'http://localhost:8000',
-	paypalClientId: 'AX91wn4oiEHoD-g0bL2yFguc1WC4PMcffleEPMOTEoyln_G5f0C99doFsSFIPoIzh2KDrwYT0P7KPBGN'
+	production: true,
+	server: 'https://makerking.net/api',
+	paypalClientId: 'AU4R1fPuVPnmNPSYuZDNJyGof5f2OsGcT1fIzP-HrMjQ8HmZN4I3WCmUymJ51HA6ptO3pMjA-NHcb79C'
 }
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
